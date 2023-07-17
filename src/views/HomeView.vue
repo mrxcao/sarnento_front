82fdcf0e62aa3b1d4ce05f1a8c5af8ecfa1a8a30
MrXcao <francisco.ramos@gmail.com>
2023-07-17 10:20:05 -0300

    home invite bot

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="home">
-      Home
-    </div>
+  <div class="home">
+    <h1>Bem-vindo ao Sarnento Bot</h1>
+    <p>Mais que um bot, um amigo que interage no seu chat !</p>
+    <button @click="inviteBot">Convidar o Bot</button>
+  </div>
   </template>
   
   <script>
@@ -11,7 +13,14 @@
     name: 'HomeView',
     components: {
      
-    }
+    },
+    methods: {
+      inviteBot() {
+        console.log('invitado')        
+        const inviteUrl = 'https://discord.com/oauth2/authorize?client_id=722913076344782858&scope=bot&permissions=549755289087';
+        window.open(inviteUrl, '_blank');
+      },
+    },
   }
   </script>
   
