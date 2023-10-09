9d573ebf172f5c424d26bc87b442a3c0905a35dd
MrXcao <francisco.ramos@gmail.com>
2023-10-09 11:07:43 -0300

    hotfix build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { resolve, win32, posix } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { resolve, win32, posix } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
   root: './',
-  build: {  
-    build:'dist',
+  build: {
+    build: { outDir: 'dist' },
     reportCompressedSize: false,
     chunkSizeWarningLimit: 730, // vuetify
     rollupOptions: {
@@ -15,7 +14,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         // nested: resolve(__dirname, 'sarnento/index.html'),
       },
-      
+
     },
   },
-})
+});
